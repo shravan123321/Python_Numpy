a049e9ed005552697aa67f97e7a5b9bd10f3ff2b
Shravan7033 <kumarshravan40478@gmail.com>
2025-11-04 18:30:17 +0530

    One Dimensional Array

---
+++ onedarray/q1.py
@@ -0,0 +1,7 @@
+# define the one dimensinal array
+import numpy as np
+arr=np.array([1,2,3,4])
+print("array data: ",arr)
+print("check the type array: ",type(arr))
+print("check the array dimensinal: ",arr.ndim)
+print("array shap: ",arr.shape)

b82ba0fea5c0a5c7958de0399b9e2719c812389c
Shravan7033 <kumarshravan40478@gmail.com>
2025-11-04 18:27:38 +0530

    Linear Algebra

---
+++ linearalgebra/q1.py
@@ -0,0 +1,19 @@
+# define the linear algebra 
+import numpy as np
+
+arr1=np.random.randint(0,10,(3,3))
+print(arr1)
+# cos digree value
+cos=np.cos(arr1)
+print("cost value print: ",cos)
+# sine digree value 
+sin=np.sin(arr1)
+print('\n',"sine value: ",sin)
+
+# np.log()
+# np.power()
+# np.men()
+# np.max()
+#np.main()
+
+# there is a diffrent value print the number in th linear algebra 
